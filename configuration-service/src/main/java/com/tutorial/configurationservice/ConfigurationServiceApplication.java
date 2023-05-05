84bcf5c338a7dfaaf259716da18c1cf809d67aee
cparamu <132406113+cparamu@users.noreply.github.com>
2023-05-04 19:14:10 -0500

    set correct config server annotation

--- configuration-service/src/main/java/com/tutorial/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/tutorial/configurationservice/ConfigurationServiceApplication.java
@@ -1,11 +1,11 @@
 package com.tutorial.configurationservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
 	public static void main(String[] args) {
